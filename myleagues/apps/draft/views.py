24da44ad00a4da967f442be18478767e72282e10
Chris Brakebill <chris.brakebill@gmail.com>
2011-08-09 15:20:23 -0400

    Renamed view from draft to draft_page and commented out some boilerplate crap

--- myleagues/apps/draft/views.py
+++ myleagues/apps/draft/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render_to_response
 
-def draft(request):
+def draft_page(request, league_id):
 	return render_to_response('draft.html')
 	
 	
 # Displays the current users team on the left sidebar
-def my_team(request):
+#def my_team(request):
 
 
 # Displays the league's draft board in the middle of the screen
-def draft_board(request):
+#def draft_board(request):
 
 # Displays the previous, current, and next picks above the draft board
-def pick_summary(request):
+#def pick_summary(request):
 
 # Displays the available players (dependent on search field and some boolean position selectors
-def available_players(request):
+#def available_players(request):
