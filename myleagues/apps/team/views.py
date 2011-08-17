63469b387f45f90af5ab34eca8fd279d5a17138c
Chris Brakebill <chris.brakebill@gmail.com>
2011-08-16 20:09:43 -0400

    Edited team views.py to display with the team.html template

--- myleagues/apps/team/views.py
+++ myleagues/apps/team/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 # Create your views here.
-def team_page(request):
-	return render_to_response("team.html")
+def team_page(request, team_id, league_id):
+	return render_to_response("team.html", context_instance=RequestContext(request))
