ebfb043299d7aad0bb99ac5eced385f5886411c9
Chris Brakebill <chris.brakebill@gmail.com>
2011-08-25 13:40:40 -0400

    Created owner app and added CSS file for the profile/owner page

---
+++ myleagues/apps/owner/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from idios.models import ProfileBase
+#from league.models import League, Transaction
+#from team.models import Team
+
+#class Owner(ProfileBase):
+#	name = models.CharField(max_length=30)
+#	slug = models.SlugField()
+#	leagues = models.ManyToManyField(League)
+	
+	
