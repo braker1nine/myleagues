bd7adf3474ec75fe4f45a62a5eaf7253dd82eddf
Chris Brakebill <chris.brakebill@gmail.com>
2011-08-28 20:41:11 -0400

    Fixed render to response to include the request context

--- myleagues/apps/league/views.py
+++ myleagues/apps/league/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 # Create your views here.
 def league_page(request, league_id):
-	return render_to_response("league.html")
+	return render_to_response("league.html", context_instance=RequestContext(request))
