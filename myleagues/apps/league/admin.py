3d6a634a6425e4bca5f5b06e0b312757a6d7502d
Chris Brakebill <chris.brakebill@gmail.com>
2011-08-31 18:53:52 -0400

    Registered all of my apps and models with the admin site

---
+++ myleagues/apps/league/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from league.models import Owner, Player, Team, Scoring, League, Transaction, trade, team_trade, add, drop, add_drop, Roster
+
+admin.site.register(Owner)
+admin.site.register(Player)
+admin.site.register(Team)
+admin.site.register(Scoring)
+admin.site.register(League)
+admin.site.register(Transaction)
+admin.site.register(trade)
+admin.site.register(team_trade)
+admin.site.register(add)
+admin.site.register(drop)
+admin.site.register(add_drop)
+admin.site.register(Roster)
