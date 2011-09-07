65c4a8b52f4f49ee7e47d19470a34f82e5be4a56
Chris Brakebill <chris.brakebill@gmail.com>
2011-09-07 09:40:19 -0400

    created initial draft models

--- myleagues/apps/draft/models.py
+++ myleagues/apps/draft/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from league.models import Player, League, Team, Owner
 
 # Create your models here.
+
+class DraftPlayerProxy(models.Model):
+	player = models.ForeignKey(Player)
+	owner = models.ForeignKey(Owner)
+	rank = models.IntegerField()
+	available = models.BooleanField()
+	
+	class Meta:
+		unique_together = ("rank", "owner")
+
+class Draft(models.Model):
+	league = models.OneToOneField(League, related_name="draft")
+
+class Round(models.Model):
+	round = models.IntegerField(editable=False)
+	draft = models.ForeignKey(Draft)
+
+class Pick(models.Model):
+	pick = models.IntegerField(editable=False)
+	player = models.ForeignKey(PlayerProxy, unique=True, blank=True)
+	team = models.ForeignKey(Team)
+	round = models.ForeignKey(Round)
+	
